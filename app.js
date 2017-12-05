e939abb9a80de38fedf460eaabb1d321e2ed0722
yukixoma <yukixoma08@gmail.com>
2017-12-05 13:14:37 +0700

    commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// Initialize
+var express     = require("express");
+var bodyParser  = require("body-parser");
+var cors        = require("cors");
+
+// Create app, setting up cors, bodyParser...
+var app = module.exports = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+
+//GET call to return JSON
+app.get('/time/:timeString',function(req,res,next){
+    console.log("URL Working");
+    var timeString = req.params.timeString;
+//default format date
+    var date       = new Date (timeString); 
+    var month      = parseInt(date.getMonth()) + 1;   
+    var natural    = date.getDate() + " " + month + " " + date.getFullYear();
+    var unix       = Math.round(+date/1000);         
+// if input is unixdate format    
+    if (isNaN(date.getDate())) {
+        var unixDate   = new Date (+timeString*1000)
+        month          = parseInt(unixDate.getMonth()) + 1; 
+        natural        = unixDate.getDate() + " " + month + " " + unixDate.getFullYear();
+        unix           = +timeString;
+    }
+//response result
+    res.json({"unix": unix, "natural": natural});
+    
+})
+
+
+
+
+app.listen(3000,function(){
+    console.log("Working");
+});
